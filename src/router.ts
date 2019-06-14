228e9d93d06ff5ae8cd7c26d29dd444159a8f184
yuands <yuands@neusoft.com>
2019-06-14 09:55:51 +0800

    create project

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: resolve => require(["@/views/layout"], resolve),
+      children: [
+        {
+          path: "about",
+          name: "about",
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () =>
+            import(/* webpackChunkName: "about" */ "./views/About.vue")
+        }
+      ]
+    },
+    {
+      path: "/hello",
+      name: "hello",
+      component: resolve => require(["@/components/HelloWorld.vue"], resolve)
+    }
+  ]
+});
